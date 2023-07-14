2b79c04f53d892b653205b2036b9120e3f62eff9
Sandarbh Bajpai <118759857+Bajpaisandarbh@users.noreply.github.com>
2023-07-14 11:12:59 +0530

    Random Choice Picker

--- script.js
+++ script.js
@@ -1,15 +1,69 @@
-const loadText = document.querySelector(".loading-text");  
-const bg = document.querySelector(".bg");  
-let load = 0;  
-const blurring = () => {  
- load++;  
- if (load > 99) clearInterval(int);  
- loadText.innerText = `${load}%`;  
- loadText.style.opacity = scale(load, 0, 100, 1, 0);  
- bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;  
-};
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
-let int = setInterval(blurring, 30);
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
+})
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+}
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+	
+	if (randomTag !== undefined) {
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+	}
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
